6579206c3ef9f59d0e62c6419cde419e05001e74
laohei <2062741737@qq.com>
2024-01-14 21:30:37 +0800

    新的测试

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -1,5 +1,5 @@
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("Hello !GitHub!114514");
+        System.out.println("Hello !GitHub!1919810");
     }
 }
